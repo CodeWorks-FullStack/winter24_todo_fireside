f0fd3ac2132ccfd69eb5131811d024f063f908ff
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-20 18:02:45 -0700

    put and delete

--- src/controllers/TodosController.js
+++ src/controllers/TodosController.js
@@ -7,6 +7,8 @@ export class TodosController extends BaseController {
     this.router
       .get('', this.getTodos)
       .post('', this.createTodo)
+      .put('/:todoId', this.updateTodo)
+      .delete('/:todoId', this.destroyTodo)
   }
 
   /**
@@ -37,4 +39,36 @@ export class TodosController extends BaseController {
       next(error)
     }
   }
+  /**
+  * @param {import("express").Request} request
+  * @param {import("express").Response} response
+  * @param {import("express").NextFunction} next
+  */
+
+  async updateTodo(request, response, next) {
+    try {
+      const todoData = request.body
+      const todoId = request.params.todoId
+      const todo = await todosService.updateTodo(todoId, todoData)
+      response.send(todo)
+    } catch (error) {
+      next(error)
+    }
+  }
+  /**
+  * @param {import("express").Request} request
+  * @param {import("express").Response} response
+  * @param {import("express").NextFunction} next
+  */
+
+  async destroyTodo(request, response, next) {
+    try {
+      const todoId = request.params.todoId
+      const message = await todosService.destroyTodo(todoId)
+      response.send(message)
+    } catch (error) {
+      next(error)
+    }
+  }
 }
+
--- src/services/TodosService.js
+++ src/services/TodosService.js
@@ -1,6 +1,9 @@
 import { dbContext } from "../db/DbContext.js"
+import { BadRequest } from "../utils/Errors.js"
 
 class TodosService {
+
+
   async getTodos() {
     const todos = await dbContext.Todos.find()
     return todos
@@ -8,6 +11,36 @@ class TodosService {
   async createTodo(todoData) {
     const todo = await dbContext.Todos.create(todoData)
     return todo
+  }
+
+  async getTodoById(todoId) {
+    const todo = await dbContext.Todos.findById(todoId)
+    if (!todo) {
+      throw new BadRequest(`Invalid id: ${todoId}`)
+    }
+    return todo
+  }
+
+  async updateTodo(todoId, todoData) {
+    const todoToUpdate = await this.getTodoById(todoId)
+
+    todoToUpdate.description = todoData.description || todoToUpdate.description
+    todoToUpdate.completed = todoData.completed == undefined ?
+      todoToUpdate.completed
+      :
+      todoData.completed
+
+    await todoToUpdate.save()
+
+    return todoToUpdate
+  }
+
+  async destroyTodo(todoId) {
+    const todoToDestroy = await this.getTodoById(todoId)
+
+    await todoToDestroy.deleteOne()
+
+    return `${todoToDestroy.description} is no more`
   }
 }
 
